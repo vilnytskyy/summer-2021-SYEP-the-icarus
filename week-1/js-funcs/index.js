69eca99f28d9ef06b49d6953e1c0a4bd8f5c64a4
vilnytskyy <vilnytskyy21@gmail.com>
2021-07-22 01:39:28 -0400

    Added mySome and tests

--- week-1/js-funcs/index.js
+++ week-1/js-funcs/index.js
@@ -6,31 +6,31 @@
  * @param array     should be an array if this is not run as a method.
  */
 Array.prototype.myForEach = (loopBody, array) => {
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] === undefined) continue;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === undefined) continue;
 
-        loopBody(array[i], i, array);
-    }
+    loopBody(array[i], i, array);
+  }
 };
 
 
 // FOR EACH TESTS //
 console.log("==== Testing forEach ====");
 
-// Test 1: Repl is Extra //
-console.log("== Test 1: Repl is Extra ==");
+// Test 1: Print Elements //
+console.log("== Test 1: Print Elements ==");
 //this should be const
 const testForEach = ["Repl", "is", "extra"];
 
 // func found on the MDN Web Docs page for forEach
 function logArrayElements(element, index, array) {
-    console.log('a[' + index + '] = ' + element);
+  console.log('a[' + index + '] = ' + element);
 }
 
-console.log("= forEach: =");
+console.log("= forEach =");
 testForEach.forEach(logArrayElements);
 
-console.log("= myForEach: =");
+console.log("= myForEach =");
 testForEach.myForEach(logArrayElements, testForEach);
 
 //make this an arrow function
@@ -47,22 +47,13 @@ console.log("== Test 2: Even ==");
 
 const arr1 = [1, 2, , 3];
 
-const printIsEven = number => console.log(number % 2 === 0);
+const printIsEven = number => console.log("Is", number, "even?", number % 2 === 0);
 
-console.log("= forEach: =");
+console.log("= forEach =");
 arr1.forEach(printIsEven);
 
-console.log("= myForEach: =");
+console.log("= myForEach =");
 arr1.myForEach(printIsEven, arr1);
-
-// Test 3: Print Elements //
-console.log("== Test 3: Print Elements ==");
-
-console.log("= forEach: =");
-arr1.forEach(logArrayElements);
-
-console.log("= myForEach: =");
-arr1.myForEach(logArrayElements, arr1);
 
 
 
@@ -75,20 +66,20 @@ arr1.myForEach(logArrayElements, arr1);
  * @return         a brand new array with equal length to the original.
  */
 Array.prototype.myMap = (mapping, array) => {
-    //if this is run as an instance method, bind array to this
-    if (array == undefined || array == null) {
-        array = this;
-    }
+  //if this is run as an instance method, bind array to this
+  if (array == undefined || array == null) {
+    array = this;
+  }
 
-    let newArray = [];
+  let newArray = [];
 
-    for (let i = 0; i < array.length; i++) {
-        if(array[i] === undefined) continue;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === undefined) continue;
 
-        newArray[i] = mapping(array[i], i, array);
-    }
+    newArray[i] = mapping(array[i], i, array);
+  }
 
-    return newArray;
+  return newArray;
 };
 
 
@@ -100,13 +91,13 @@ console.log("== Test 1: Squared ==");
 
 const testMap = [1, 2, 3, 4, 5];
 const square = (element) => {
-    return element * element;
+  return element * element;
 }
 
-console.log("= map: =");
+console.log("= map =");
 console.log(testMap.map(square));
 
-console.log("= myMap: =");
+console.log("= myMap =");
 console.log(testMap.myMap(square, testMap));
 //should be [1, 4, 9, 16, 25]
 
@@ -116,10 +107,10 @@ console.log("== Test 2: X2 ==");
 const arr3 = [1, 4, 9, , 16];
 const times2 = x => x * 2;
 
-console.log("= map: =");
+console.log("= map =");
 console.log(arr3.map(times2));
 
-console.log("= myMap: =");
+console.log("= myMap =");
 console.log(arr3.myMap(times2, arr3));
 
 // Test 3: Reformat Array
@@ -129,15 +120,15 @@ const kvArray = [{ key: 1, value: 10 }, { key: 2, value: 20 }, { key: 3, value: 
 
 // func found on the MDN Web Docs page for map
 const reformatArray = (obj => {
-    let rObj = {};
-    rObj[obj.key] = obj.value;
-    return rObj;
+  let rObj = {};
+  rObj[obj.key] = obj.value;
+  return rObj;
 })
 
-console.log("= map: =");
+console.log("= map =");
 console.log(kvArray.map(reformatArray));
 
-console.log("= myMap: =");
+console.log("= myMap =");
 console.log(kvArray.myMap(reformatArray, kvArray));
 
 
@@ -151,24 +142,24 @@ console.log(kvArray.myMap(reformatArray, kvArray));
  * @return                the new array
  */
 Array.prototype.myFilter = (filterFunction, array) => {
-    if (array == undefined) {
-        array = this;
+  if (array == undefined) {
+    array = this;
+  }
+
+  let arr = [];
+  let arr_i = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === undefined) continue;
+
+    if (filterFunction(array[i], i, array)) {
+      arr[arr_i] = array[i];
+      arr_i++;
     }
+  }
 
-    let arr = [];
-    let arr_i = 0;
-
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] === undefined) continue;
-
-        if (filterFunction(array[i], i, array)) {
-            arr[arr_i] = array[i];
-            arr_i++;
-        }
-    }
-
-    return arr;
-}
+  return arr;
+};
 
 // FILTER TESTS //
 console.log("==== Testing Filter ====");
@@ -179,12 +170,12 @@ console.log("== Test 1: Even ==");
 let testFilter = [1, 2, 3, 4, 5];
 const isEven = number => number % 2 === 0;
 
-console.log("filter: ");
+console.log("= filter =");
 console.log(testFilter.filter(isEven));
 // should be [2, 4]
 
-console.log("myFilter: ");
-console.log(testFilter.filter(isEven, testFilter));
+console.log("= myFilter =");
+console.log(testFilter.myFilter(isEven, testFilter));
 
 // Test 2: Limit
 console.log("== Test 2: Limit ==");
@@ -192,10 +183,10 @@ console.log("== Test 2: Limit ==");
 const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
 const limit = word => word.length > 6;
 
-console.log("filter: ");
+console.log("= filter =");
 console.log(words.filter(limit));
 
-console.log("myFilter: ");
+console.log("= myFilter =");
 console.log(words.myFilter(limit, words));
 
 // Test 3: Prime
@@ -205,19 +196,21 @@ const arr4 = [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 
 // func found on the MDN Web Docs page for filter
 function isPrime(num) {
-    for (let i = 2; num > i; i++) {
-        if (num % i == 0) {
-            return false;
-        }
+  for (let i = 2; num > i; i++) {
+    if (num % i == 0) {
+      return false;
     }
-    return num > 1;
+  }
+  return num > 1;
 }
 
-console.log("filter: ");
+console.log("= filter =");
 console.log(arr4.filter(isPrime));
 
-console.log("myFilter: ");
+console.log("= myFilter =");
 console.log(arr4.myFilter(isPrime, arr4));
+
+
 
 /**
  * SOME
@@ -227,8 +220,45 @@ console.log(arr4.myFilter(isPrime, arr4));
  * @param array     the array that should be looped over
  * @return          true or false.
  */
-function mySome(loopBody, array) { }
+Array.prototype.mySome = (loopBody, array) => {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === undefined) continue;
+
+    if (loopBody(array[i], i, array)) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+
+// SOME TESTS //
 console.log("==== Testing Some ====");
+
+// Test 1: Even
+console.log("== Test 1: Even ==");
+
+const arr5 = [1, 2, 3, 4, 5];
+
+console.log("= some =");
+console.log(arr5.some(isEven));
+
+console.log("= mySome =");
+console.log(arr5.mySome(isEven, arr5));
+
+// Test 2: All Odds
+console.log("== Test 2: All Odds ==");
+
+const arr5b = [1, 5, 7, 3, 9, 13];
+
+console.log("= some =");
+console.log(arr5b.some(isEven));
+
+console.log("= mySome =");
+console.log(arr5b.mySome(isEven, arr5b));
+
+
 
 /**
  * EVERY
@@ -242,6 +272,8 @@ function myEvery(loopBody, array) { }
 
 Array.prototype.every = myEvery;
 console.log("==== Testing every ====");
+
+
 
 /**
  * REDUCE
@@ -259,13 +291,15 @@ Array.prototype.reduce = myReduce;
 console.log("==== Testing Reduce ====");
 let testReduce = [-3, 3, 9];
 console.log("Sum: " + myReduce(function (sum, x) {
-    return sum + x;
+  return sum + x;
 }, 100, testReduce));
 //should print Sum: 109
 console.log("Min: " + myReduce(function (min, x) {
-    return (min < x) ? min : x;
+  return (min < x) ? min : x;
 }, 0, testReduce));
 //should print Min: -3
+
+
 
 /**
  * INCLUDES
@@ -276,8 +310,8 @@ console.log("Min: " + myReduce(function (min, x) {
  * @return       true or false
  */
 function myIncludes(value, array) {
-    array = array || this;
-    //code here
+  array = array || this;
+  //code here
 }
 
 Array.prototype.includes = myIncludes;
@@ -288,6 +322,8 @@ let testIncludes = [6, 7, 8];
 console.log("Includes 6: " + myIncludes(6, testIncludes));
 console.log("Includes 10: " + testIncludes.includes(10));
 
+
+
 /**
  * INDEXOF
  * 
@@ -297,13 +333,15 @@ console.log("Includes 10: " + testIncludes.includes(10));
  * @param haystack   the array to look in
  */
 function myIndexOf(needle, haystack, beginFrom) {
-    beginFrom = beginFrom || 0; //default value
-    haystack = haystack || this;
-    //code here
+  beginFrom = beginFrom || 0; //default value
+  haystack = haystack || this;
+  //code here
 }
 
 Array.prototype.indexOf = myIndexOf;
 console.log("==== Testing indexOf ====");
+
+
 
 /**
  * PUSH
@@ -321,13 +359,15 @@ Array.prototype.push = myPush;
 let testPush = ["why do we use"];
 console.log(myPush("repl", testPush));
 
+
+
 /**
  * LASTINDEXOF
  * 
  * Finds the last occurence of an element.
  */
 function myLastIndexOf(needle, haystack, beginFrom) {
-    haystack = haystack || this;
+  haystack = haystack || this;
 }
 
 Array.prototype.lastIndexOf = myLastIndexOf;
@@ -338,14 +378,18 @@ let testLast = ["P", "Q", "Q", "P", "P"];
 console.log("Last P: " + myLastIndexOf("P", testLast));
 console.log("Last Q: " + testLast.lastIndexOf("Q"));
 
+
+
 /**
  * KEYS
  * 
  * Gets an array of object properties
  */
 function grabKeys(obj) {
-    //hint: lookup for-in loops
+  //hint: lookup for-in loops
 }
+
+
 
 /**
  * VALUES
